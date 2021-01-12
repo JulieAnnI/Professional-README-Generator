57fe5cfaee73aa24dfa5cf767ed0444621511fd2
Julie Ann <julieanniwinski@gmail.com>
2021-01-12 18:01:52 -0500

    Fixed comments on all pages

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// TODO: Include packages needed for this application
+//Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
@@ -6,7 +6,7 @@ const writeFileAsync = util.promisify(writeToFile);
 const generateMarkdown = require("./utils/generateMarkdown.js");
 
 
-// TODO: Create an array of questions for user input
+//Array of questions for user input
 const questions = [
     {
         type:"input",
@@ -71,7 +71,7 @@ const questions = [
     },
 ];
 
-// TODO: Create a function to write README file
+//Function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, error => {
         if (error) {
@@ -81,7 +81,7 @@ function writeToFile(fileName, data) {
     });
 }
 
-// TODO: Create a function to initialize app
+//Function to initialize app
 async function init() {
     try {
         const responses = await inquirer.prompt(questions);
